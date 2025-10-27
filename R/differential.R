6b4d07186242b50c0ab585b090c9f2fdddd9b7c1
vicciv1623 <vickyhaeunjung@gmail.com>
2025-10-26 22:22:23 -0400

    fix DOCUMENTATION notes

--- R/differential.R
+++ R/differential.R
@@ -1,4 +1,14 @@
-# build polynomial class
+#' Differential class
+#'
+#' @param equation a character
+#'
+#' Building basis for defining an ODE
+#' @section Properties:
+#' \describe{
+#'  \item{equation}{The basis for an ODE}
+#' }
+#'
+#' @export
 Differential<-S7::new_class("Differential",
                       properties=list(
                         equation=S7::class_character
